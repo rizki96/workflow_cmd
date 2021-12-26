35fc62c04c8aa64759a8bc77cd5f92f8ae3a6fae
Iskandar Rizki <iskandar.rizki@gmail.com>
2021-12-27 03:47:13 +0700

    add command-line description

--- lib/workflow_cmd.ex
+++ lib/workflow_cmd.ex
@@ -5,7 +5,7 @@ defmodule WorkflowCmd do
 
   @impl Bakeware.Script
   def main([]) do
-    IO.puts("\nRunning the JSON workflow DSL
+    IO.puts("\nRunning the JSON workflow DSL via command-line
 \nCommand: workflow_cmd <json workflow file path / URL> [--verbose]
 ")
   end
