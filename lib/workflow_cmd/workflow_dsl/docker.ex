a6226c29ed7bd940b26488f8279907d80f46cb6d
iskandar.rizki <iskandar.rizki@rumahsiapkerja.com>
2022-01-23 22:18:16 +0700

    support for config_url for docker compose

--- lib/workflow_cmd/workflow_dsl/docker.ex
+++ lib/workflow_cmd/workflow_dsl/docker.ex
@@ -146,7 +146,18 @@ defmodule WorkflowDsl.Docker do
       with true <- Map.has_key?(parameters, "config_path") do
         parameters["config_path"]
       else
-        _ -> ""
+        _ ->
+          with true <- Map.has_key?(parameters, "config_url") do
+            if String.starts_with?(parameters["config_url"], ["http://", "https://"]) do
+              {:ok, content} = Req.request(:get, parameters["config_url"])
+              File.write!(Path.basename(parameters["config_url"]), content.body)
+              Path.basename(parameters["config_url"])
+            else
+              Path.basename(parameters["config_url"])
+            end
+          else
+            _ -> "docker-compose.yml"
+          end
       end
 
     command =
