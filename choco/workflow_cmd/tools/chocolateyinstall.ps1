21720966a861623df8847d7998db451678eb867f
rizki96 <iskandar.rizki@gmail.com>
2022-04-16 05:07:01 +0700

    fix licenseUrl, fix install arguments

--- choco/workflow_cmd/tools/chocolateyinstall.ps1
+++ choco/workflow_cmd/tools/chocolateyinstall.ps1
@@ -1,13 +1,14 @@
 ﻿
 $ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $packageArgs = @{
-  packageName   = 'workflow_cmd'
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
   url = 'https://github.com/rizki96/workflow_cmd/releases/download/v0.4.5/workflow_cmd-0.4.5.zip'
   checksum = '1A95C565F36AC18EEAE136D765FE3CB24EB6023F66C7FC69286227F62470F0AB'
+  checksumType64 = 'sha256'
 }
 
-Install-ChocolateyZipPackage -PackageName $packageArgs.packageName `
- -Url $packageArgs.url `
- -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" `
- -Checksum $packageArgs.checksum `
- -ChecksumType 'SHA256'
+Install-ChocolateyZipPackage @packageArgs
--- choco/workflow_cmd/tools/chocolateyuninstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-$packageArgs = @{
-  packageName   = 'workflow_cmd'
-  zipFileName   = 'workflow_cmd-0.4.5.zip'
-}
-
-Uninstall-ChocolateyZipPackage -PackageName $packageArgs.packageName `
-  -ZipFileName $packageArgs.zipFileName
