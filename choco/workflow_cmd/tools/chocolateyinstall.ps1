f846987e0e47575b9d900ffcfaf6aa4763d05183
rizki96 <iskandar.rizki@gmail.com>
2022-04-16 09:31:20 +0700

    fix checksumType field

--- choco/workflow_cmd/tools/chocolateyinstall.ps1
+++ choco/workflow_cmd/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   unzipLocation  = $toolsDir
   url = 'https://github.com/rizki96/workflow_cmd/releases/download/v0.4.5/workflow_cmd-0.4.5.zip'
   checksum = '1A95C565F36AC18EEAE136D765FE3CB24EB6023F66C7FC69286227F62470F0AB'
-  checksumType64 = 'sha256'
+  checksumType = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
