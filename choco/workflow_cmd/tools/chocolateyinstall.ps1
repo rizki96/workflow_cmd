c36c8ffc42a74f4e1145064c39e891710de3f36c
rizki96 <iskandar.rizki@gmail.com>
2022-06-12 11:38:42 +0700

    add version text in binary

--- choco/workflow_cmd/tools/chocolateyinstall.ps1
+++ choco/workflow_cmd/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  url = 'https://github.com/rizki96/workflow_cmd/releases/download/v0.4.5/workflow_cmd-0.4.5.zip'
-  checksum = '1A95C565F36AC18EEAE136D765FE3CB24EB6023F66C7FC69286227F62470F0AB'
+  url = 'https://github.com/rizki96/workflow_cmd/releases/download/v0.4.6/workflow_cmd-0.4.6.zip'
+  checksum = '85DBD9DAC9C56A272BE18FEC1B3CBB51A93E1B7BAF09B60B762F159502A67E02'
   checksumType = 'sha256'
 }
 
--- lib/workflow_cmd.ex
+++ lib/workflow_cmd.ex
@@ -6,6 +6,7 @@ defmodule WorkflowCmd do
   @impl Bakeware.Script
   def main([]) do
     IO.puts("\nRunning the JSON workflow DSL via command-line
+worflow_cmd ver. #{Application.spec(:workflow_cmd, :vsn)}
 \nCommand: workflow_cmd <json workflow file path / URL> [--verbose]
 Ex.: > workflow_cmd https://raw.githubusercontent.com/rizki96/workflow_dsl/master/examples/workflow3.json --verbose
 ")
