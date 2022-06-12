a1801b21c0d30fb60e9dbc360b3410ae607287c7
rizki96 <iskandar.rizki@gmail.com>
2022-06-12 12:48:54 +0700

    update checksum

--- choco/workflow_cmd/tools/chocolateyinstall.ps1
+++ choco/workflow_cmd/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   url = 'https://github.com/rizki96/workflow_cmd/releases/download/v0.4.6/workflow_cmd-0.4.6.zip'
-  checksum = '85DBD9DAC9C56A272BE18FEC1B3CBB51A93E1B7BAF09B60B762F159502A67E02'
+  checksum = '18F93ABB842E7B58BB0199BDE28EB95B77D48E3275B264BEF010EFDD0CC7884E'
   checksumType = 'sha256'
 }
 
